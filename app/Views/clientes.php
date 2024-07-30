3645f556653b70906b8b7f4f406c0bbc2a89bc4e
GarySnts <gsami.santos@hotmail.com>
2024-07-29 21:26:29 -0600

    reparacion de links de nav

--- app/Views/clientes.php
+++ app/Views/clientes.php
@@ -9,10 +9,10 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
-<body class="container bg-black">
+<body class="container bg-black text-white">
     <nav class="navbar navbar-expand-lg bg-danger" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/fs2024/db_telefonica/public/">Raiz</a>
+            <a class="navbar-brand" href="http://localhost/fs2024/bd_hotel/public/">Raiz</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- app/Views/hoteles.php
+++ app/Views/hoteles.php
@@ -12,7 +12,7 @@
 <body class="container">
     <nav class="navbar navbar-expand-lg bg-success" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/fs2024/db_telefonica/public/">Raiz</a>
+            <a class="navbar-brand" href="http://localhost/fs2024/bd_hotel/public/">Raiz</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
